d23c095ec6aecdce9443acd1fa73a69c36cdbed5
MKS-Student <mks.student@hackreactor.com>
2017-04-04 12:34:24 -0700

    Implement friendFetch

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,19 +2,26 @@ var app = {};
 
 app.friends = [];
 
-app.init = function(){};
-
-app.message = {
-    username: 'Mel Brooks',
-    text: 'It\'s good to be the king',
-    roomname: 'lobby'
+app.init = function(){
+  app.fetch()
 };
 
-app.send = function(message){
+app.message = function(name, text, room) {
+  var message = {
+    username: name,
+    text: text,
+    roomname: room
+  };
+    return message;
+};
+
+app.server = 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages';
+
+app.send = function(message) {
 
     return $.ajax({
         // This is the url you should use to communicate with the parse API server.
-        url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+        url: app.server,
         type: 'POST',
         data: JSON.stringify(message),
         contentType: 'application/json',
@@ -27,46 +34,62 @@ app.send = function(message){
         }
     });
 };
-app.server = 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages';
 
-app.fetch = function(){
-    $.get('http://parse.sfs.hackreactor.com/chatterbox/classes/messages', function(data){
-      console.log(data.results);
+app.fetch = function(args) {
+    //$.get(app.server,
+    return $.ajax({
+       url: app.server,
+       type: 'GET',
+       data: args || 'order=-createdAt',
+       //data:'where={"username":{"$in":["matt"]}}',
+       success: function(data) {
+         var message = JSON.stringify(data.results[0]);
+         var messagesArray = data.results;
 
-      var message = JSON.stringify(data.results[0]);
-      var messagesArray = data.results;
-
-      for (var i = 0; i < messagesArray.length; i++) {
-        var user = messagesArray[i].username;
-        var userLink = '<button class=' + user + '>' + user + '</button>';
-        $('#chats').append('<p>'+userLink+'  '+messagesArray[i].text+'</p>');
+         for (var i = 0; i < messagesArray.length; i++) {
+           var username = messagesArray[i].username;
+           var userLink = '<button class=' + username + '>' + username + '</button>';
+           $('#chats').append('<p>'+userLink+'  '+messagesArray[i].text+'</p>');
+         }
+         $('button').on('click', app.handleUsernameClick.bind(event));
+      },
+      error: function(data) {
+        console.error('chatterbox: Failed to recieve messages', data);
       }
-      $('button').on('click', function(event) {
-          console.log(event.target.className);
-          app.friends.push(event.target.className);
-      });
-    });
+   });
 };
 
-app.fetch();
+app.init();
 
 app.clearMessages = function(){
   $('#chats').empty();
 };
 
-app.renderMessage = function(name, text, room) {
-  var message = {
-    username: name,
-    text: text,
-    roomname: room
-  };
+app.renderMessage = function(message) {
   $('#chats').append('<p>' + message.text + '</p>');
+  return message;
 };
 
 app.renderRoom = function(room) {
   $('#roomSelect').append('<div id=' + room + '></div>');
 };
 
-app.handleUsernameClick = function(){
+app.handleUsernameClick = function(event) {
+
+  if (!app.friends.includes(event.target.className)) {
+    var name = event.target.className;
+    var friendElement = '<li id="' + name + '">' + name + '</li>';
+    $('.friends').append(friendElement);
+    app.friends.push(event.target.className);
+    console.log(name);
+    $('#' + name).on('click', app.friendFetch.bind(event));
+  }
+};
+
+app.friendFetch = function(event) {
+  var name = event.target.id;
+  $('#chats').children().remove();
+  var data = 'where={"username":{"$in":["' + name + '"]}}';
+  app.fetch(data);
 
 }
