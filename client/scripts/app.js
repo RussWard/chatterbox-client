f82ca55a10e009ffd0e952ada367be612aea491c
MKS-Student <mks.student@hackreactor.com>
2017-04-04 16:00:51 -0700

    Implement addFriends, and sendMessage

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,7 +3,8 @@ var app = {};
 app.friends = [];
 
 app.init = function(){
-  app.fetch()
+  app.fetch();
+  app.sendButton();
 };
 
 app.message = function(name, text, room) {
@@ -36,15 +37,16 @@ app.send = function(message) {
 };
 
 app.fetch = function(args) {
-    //$.get(app.server,
+
     return $.ajax({
        url: app.server,
        type: 'GET',
        data: args || 'order=-createdAt',
-       //data:'where={"username":{"$in":["matt"]}}',
        success: function(data) {
-         var message = JSON.stringify(data.results[0]);
-         var messagesArray = data.results;
+         console.log(data.results);
+         var cleanData = app.secure(data);
+         var message = JSON.stringify(cleanData.results[0]);
+         var messagesArray = cleanData.results;
 
          for (var i = 0; i < messagesArray.length; i++) {
            var username = messagesArray[i].username;
@@ -52,6 +54,16 @@ app.fetch = function(args) {
            $('#chats').append('<p>'+userLink+'  '+messagesArray[i].text+'</p>');
          }
          $('button').on('click', app.handleUsernameClick.bind(event));
+         $('form').on('click', '#sendMessageButton', function(event) {
+           event.preventDefault();
+           var name = $('#input-username').val();
+           var text = $('#input-text').val();
+           var room = $('#input-roomname').val();
+           var message = app.message(name, text, room);
+
+           app.send(app.renderMessage(message));
+
+         });
       },
       error: function(data) {
         console.error('chatterbox: Failed to recieve messages', data);
@@ -59,7 +71,7 @@ app.fetch = function(args) {
    });
 };
 
-app.init();
+
 
 app.clearMessages = function(){
   $('#chats').empty();
@@ -92,4 +104,42 @@ app.friendFetch = function(event) {
   var data = 'where={"username":{"$in":["' + name + '"]}}';
   app.fetch(data);
 
-}
+};
+
+app.sendButton = function() {
+  // $('#input-text').on('click', function(event) {
+  //  //  event.preventDefault();
+  //  console.log('hey');
+  //  console.log($('#input-text').val());
+  // });
+
+};
+
+
+app.secure = function(obj) {
+  for (var i = 0; i < obj.results.length; i++) {
+    if (obj.results[i].username === undefined) {
+      obj.results.splice(i, 1);
+    } else  if (obj.results[i].username.match('<.*>')) {
+      obj.results.splice(i, 1);
+    }
+    // if (obj.results[i].text.match('<.*>')) {
+    //   obj.results.splice(i, 1);
+    // }
+    // if (obj.results[i].roomname.match('<.*>')) {
+    //   obj.results.splice(i, 1);
+    // }
+    // if (obj.results[i].username.match('function()')) {
+    //   obj.results.splice(i, 1);
+    // }
+    // if (obj.results[i].text.match('function()')) {
+    //   obj.results.splice(i, 1);
+    // }
+    // if (obj.results[i].roomname.match('function()')) {
+    //   obj.results.splice(i, 1);
+    // }
+  }
+  return obj;
+};
+
+app.init();
